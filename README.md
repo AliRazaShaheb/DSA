f3bafc8c9d147afb9d20ac031cfa66b64118be1d
AliRazaShaheb <arz.shaheb@gmail.com>
2022-06-28 06:41:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-aqcb8d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-aqcb8d)
---
+++ index.js
@@ -0,0 +1,18 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>DATA STRUCTURE & ALGORITHM</h1>`;
+
+// Q1 - Fibonacci Number
+// what is Fibonacci number ?
+// my answer- Fibonacci number is sequece of numbers which is a sum of two preceding number
+// google - a series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding   numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+// example - [0,1,1,2,3,5,8,13]
+
+function Fibonacci(n = 1) {
+  console.log('Fibonacci');
+}
+
+Fibonacci();
