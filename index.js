16d252fb037fcce2e7edc300731fb5b8ac37c9bb
AliRazaShaheb <arz.shaheb@gmail.com>
2022-06-28 16:11:28 +0530

    1 file modified

--- index.js
+++ index.js
@@ -68,4 +68,33 @@ function isPrimeNum(n) {
   return 'yes this is prime number';
 }
 
-console.log(isPrimeNum(1));
+// console.log(isPrimeNum(1));
+
+// Q4 - Power of Two
+// What is Power of Two
+// power of two is the number which is multiplies of its value;
+// i.e.(id est - latin work means 'that is') : n = 2n [2PO2 = 2x2], [4PO2 = 4x4]
+//google-wikipedia:  A power of two is a number of the form 2n where n is an integer, that is, the result of exponentiation with number two as the base and integer n as the exponent.
+
+function PowerOf(n, p) {
+  let parr = [];
+  for (let i = 0; i < p; i++) {
+    parr[i] = n;
+  }
+  const multiply = parr.reduce((acc, curr) => acc * curr);
+  return [...parr, multiply];
+}
+
+// console.log(PowerOf(2, 10));
+
+function isPowerOf2(n) {
+  if (n < 2) {
+    return 'number should be greater than 2';
+  }
+  if (Math.sqrt(n) === Math.trunc(Math.sqrt(n))) {
+    return `given number '${n}' is PowerOf2`;
+  } else {
+    return `'${n}' not of PowerOf2`;
+  }
+}
+console.log(isPowerOf2(12));
